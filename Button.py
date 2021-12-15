64958a9501b6e671354108cce48f07021e934590
Karl <karl.koerner.o@gmail.com>
2021-12-15 10:43:56 +0100

    GUI with pygame, updated Button class

---
+++ Button.py
@@ -0,0 +1,23 @@
+import pygame
+
+class Button:
+    def __init__(self, color, x, y, width, height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), 0)
+        if self.text != '':
+            font = pygame.font.SysFont('comicsans', 20)
+            text = font.render(self.text, 1, (0, 0, 0))
+            screen.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + self.height/2 - text.get_height()/2))
+
+    def is_under(self, pos):
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                return True
+        return False
--- sudoku_front.py
+++ sudoku_front.py
@@ -1,130 +1,111 @@
-from sudoku_solve import main, GRID
-from tkinter import *
-from tkinter import messagebox
+import pygame, sys
+from pygame.locals import *
+from sudoku_solve import *
 from time import sleep
+from Button import Button
 
-root = Tk()
-root.title("SUDOKU")
-frame0 = LabelFrame(root, padx=5, pady=5)
-frame0.pack(padx=100, pady=10)
+pygame.init()
 
-frameA1 = LabelFrame(frame0, padx=5, pady=5)
-frameA2 = LabelFrame(frame0, padx=5, pady=5)
-frameA3 = LabelFrame(frame0, padx=5, pady=5)
+WIDTH, HEIGHT = 495, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+SQUARE = 55
+Font = pygame.font.SysFont('comicsans', 45)
+font2 = pygame.font.SysFont('comicsans', 60)
+rect_x = 55
+rect_y = 55
+BLACK = (0, 0, 0)
+GREEN = (100, 255, 100)
+WHITE = (255, 255, 255)
+GREY = (220, 220, 220)
+RED = (255, 0, 0)
 
-frameB1 = LabelFrame(frame0, padx=5, pady=5)
-frameB2 = LabelFrame(frame0, padx=5, pady=5)
-frameB3 = LabelFrame(frame0, padx=5, pady=5)
+button_fast = Button(WHITE, 90, 520, 150, 50, text='fast algorithm')
+button_visual = Button(WHITE, 250, 520, 150, 50, text='visualize algorithm')
+text_solving = font2.render('Solving in progress...', 1, (255, 0, 0))
 
-frameC1 = LabelFrame(frame0, padx=5, pady=5)
-frameC2 = LabelFrame(frame0, padx=5, pady=5)
-frameC3 = LabelFrame(frame0, padx=5, pady=5)
+def message_to_screen(msg, color, position, Font):
+    text = Font.render(msg, True, color)
+    screen.blit(text, position)
 
-frameA1.grid(row=0, column=0)
-frameA2.grid(row=0, column=1)
-frameA3.grid(row=0, column=2)
+def draw_lines():
+    for i in range(1, 10):
+        if i == 3 or i == 6:
+            pygame.draw.line(screen, BLACK, (0, i * SQUARE), (WIDTH, i * SQUARE), 5)
+            pygame.draw.line(screen, BLACK, (i*SQUARE, 0), (i * SQUARE, SQUARE * 9), 5)
+        else:
+            pygame.draw.line(screen, BLACK, (0, i * SQUARE), (WIDTH, i * SQUARE))
+            pygame.draw.line(screen, BLACK, (i*SQUARE, 0), (i * SQUARE, SQUARE * 9))
 
-frameB1.grid(row=1, column=0)
-frameB2.grid(row=1, column=1)
-frameB3.grid(row=1, column=2)
+    for i, row in enumerate(GRID):
+        for j, num in enumerate(row):
+            if num != 0:
+                message_to_screen(str(num), BLACK, (j * SQUARE + 20, i * SQUARE + 15), Font)
 
-frameC1.grid(row=2, column=0)
-frameC2.grid(row=2, column=1)
-frameC3.grid(row=2, column=2)
-
-frames = [frameA1, frameA2, frameA3, frameB1, frameB2, frameB3, frameC1, frameC2, frameC3]
-framesa = [frameA1, frameA2, frameA3]
-framesb = [frameB1, frameB2, frameB3]
-framesc = [frameC1, frameC2, frameC3]
-
-liste = []
-
-def add_boxes(frames):
-    global liste
-    for frame in frames:
-        TA1 = Text(frame, height=1, width=2)
-        TA2 = Text(frame, height=1, width=2)
-        TA3 = Text(frame, height=1, width=2)
-
-        TA1.grid(row=0, column=0)
-        TA2.grid(row=0, column=1)
-        TA3.grid(row=0, column=2)
-
-        liste.extend((TA1, TA2, TA3))
-
-    for frame in frames:
-        TB1 = Text(frame, height=1, width=2)
-        TB2 = Text(frame, height=1, width=2)
-        TB3 = Text(frame, height=1, width=2)
-
-        TB1.grid(row=1, column=0)
-        TB2.grid(row=1, column=1)
-        TB3.grid(row=1, column=2)
-
-        liste.extend((TB1, TB2, TB3))
-
-    for frame in frames:
-        TC1 = Text(frame, height=1, width=2)
-        TC2 = Text(frame, height=1, width=2)
-        TC3 = Text(frame, height=1, width=2)
-
-        TC1.grid(row=2, column=0)
-        TC2.grid(row=2, column=1)
-        TC3.grid(row=2, column=2)
-
-        liste.extend((TC1, TC2, TC3))
-
-add_boxes(framesa)
-add_boxes(framesb)
-add_boxes(framesc)
-
-
-def get_errors(input):
-    try:
-        input = int(input)
-    except ValueError:
-        messagebox.showerror("Error", "Nur ZAHLEN von 0-9!!!")
-        return None
-    if input < 0 or input > 9:
-        messagebox.showerror("Error", "Nur Zahlen von 0-9!!!")
-        return None
+def fill_pg():
+    sleep(0.25)
+    screen.fill(GREY)
+    draw_lines()
+    pos = get_pos()
+    if not pos:
+        return True
     else:
-        return input
-
-def get_input():
-    index1 = 0
-    index2 = 0
-    for text in liste:
-        input = text.get("1.0","end-1c")
-        if input == "":
-            input = 0
-        input = get_errors(input)
-        GRID[index1][index2] = int(input)
-        index2 += 1
-        if index2 == 9:
-            index2 = 0
-            index1 += 1
-        if index1 == 9:
-            index1 = 0
-    print(GRID)
-
-def print_solution():
-    for text in liste:
-        text.delete(1.0, END)
-    index = 0
-    for row in GRID:
-        for num in row:
-            liste[index].insert(END, str(num))
-            index += 1
-
-def start():
-    get_input()
-    main()
-    Label(root, text="Fertig!!!").pack()
-    print_solution()
+        i, j = pos
+    for n in range(1, 10):
+        if check(n, pos) == True:
+            GRID[i][j] = n
+            screen.fill(GREY)
+            draw_lines()
+            message_to_screen(str(n), BLACK, (j * SQUARE + 20, i * SQUARE + 15), Font)
+            pygame.draw.rect(screen, GREEN, [j * SQUARE, i * SQUARE, SQUARE, SQUARE], width=4 )
+            pygame.display.update()
+            if fill_pg():
+                return True
+            GRID[i][j] = 0
+            screen.fill(GREY)
+            draw_lines()
+            message_to_screen("0", BLACK, (j * SQUARE + 20, i * SQUARE + 15), Font)
+            pygame.display.update()
+    return False
 
 
-start = Button(root, text="START", command=start).pack()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                if rect_x >= SQUARE:
+                    rect_x -= SQUARE
+            if event.key == pygame.K_RIGHT:
+                if rect_x <= WIDTH - 2*SQUARE:
+                    rect_x += SQUARE
+            if event.key == pygame.K_UP:
+                if rect_y >= SQUARE:
+                    rect_y -= SQUARE
+            if event.key == pygame.K_DOWN:
+                if rect_y <= WIDTH - 2*SQUARE:
+                    rect_y += SQUARE
+            if event.key == pygame.K_BACKSPACE:
+                print(int(rect_y / SQUARE))
+                print(int(rect_x / SQUARE))
+                GRID[int(rect_y / SQUARE)][int(rect_x / SQUARE)] = 0
+            if event.key == K_1 or event.key == K_2 or event.key == K_3 or event.key == K_4 or event.key == K_5 or event.key == K_6 or event.key == K_7 or event.key == K_8 or event.key == K_9:
+                GRID[int(rect_y / SQUARE)][int(rect_x / SQUARE)] = int(event.unicode)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            if button_visual.is_under(pos):
+                pygame.display.update()
+                fill_pg()
+            if button_fast.is_under(pos):
+                screen.blit(text_solving, (35, 200))
+                pygame.display.update()
+                fill()
 
 
-root.mainloop()
+    screen.fill(GREY)
+    button_fast.draw(screen)
+    button_visual.draw(screen)
+    draw_lines()
+    pygame.draw.rect(screen, BLACK, [rect_x, rect_y, SQUARE, SQUARE], width=4 )
+    pygame.display.update()
